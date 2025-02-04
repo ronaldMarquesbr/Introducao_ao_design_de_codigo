0e07a017d104029df99bd1c933b6e4984e61b611
Ronald Marques <ronaldpmarques01@gmail.com>
2025-02-04 09:50:50 -0300

    Padrão de Fachada

---
+++ src/drivers/numpy_handler.py
@@ -0,0 +1,9 @@
+import numpy
+from typing import List
+
+class NumpyHandler:
+    def __init__(self):
+        self.__np = numpy
+
+    def standard_deviation(self, numbers: List[float]) -> numpy.floating:
+        return self.__np.std(numbers)
