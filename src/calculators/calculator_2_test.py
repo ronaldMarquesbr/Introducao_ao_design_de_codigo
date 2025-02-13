a35bb51a13f9b170f41bbecac78d5110f35c4126
Ronald Marques <ronaldpmarques01@gmail.com>
2025-02-13 15:18:48 -0300

    Teste unitário

--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,6 +1,7 @@
-from typing import Dict
+from typing import Dict, List
 from .calculator_2 import Calculator2
 from src.drivers.numpy_handler import NumpyHandler
+from src.drivers.interfaces.driver_interface_handler import DriverInterfaceHandler
 
 
 class MockRequest:
@@ -8,11 +9,16 @@ class MockRequest:
         self.json = body
 
 
+class MockDriverHandler(DriverInterfaceHandler):
+    def standard_deviation(self, numbers: List[float]) -> float:
+        return 3
+
+
 def test_calculate():
     mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
 
-    calculator_2 = Calculator2(NumpyHandler())
+    calculator_2 = Calculator2(MockDriverHandler())
     formatted_response = calculator_2.calculate(mock_request)
 
-    assert isinstance(formatted_response, dict)
-    assert formatted_response == {"data": {"Calculator": 2, "result": 0.08}}
+    assert isinstance(formatted_response, Dict)
+    assert formatted_response == {"data": {"Calculator": 2, "result": 0.33}}
