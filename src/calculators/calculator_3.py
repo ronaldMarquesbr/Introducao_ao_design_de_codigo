c637c38ee498366fef0b30653b7b8d708725d680
Ronald Marques <ronaldpmarques01@gmail.com>
2025-02-20 23:25:14 -0300

    Lógica da terceira calculadora

--- src/calculators/calculator_3.py
+++ src/calculators/calculator_3.py
@@ -1,11 +1,55 @@
-from typing import Dict
+from typing import Dict, List
 from flask import request as FlaskRequest
 from src.drivers.interfaces.driver_interface_handler import DriverInterfaceHandler
 
 
 class Calculator3:
     def __init__(self, driver_handler: DriverInterfaceHandler) -> None:
-        self.driver_handler = driver_handler
+        self.__driver_handler = driver_handler
 
     def calculate(self, request: FlaskRequest) -> Dict:
-        pass
+        body = request.json
+        input_data = self.__validate_body(body)
+        variance = self.__calculate_variance(input_data)
+        multiplication = self.__calculate_multiplication(input_data)
+        self.__verify_results(variance, multiplication)
+
+        formatted_response = self.__format_response(multiplication)
+
+        return formatted_response
+
+    @staticmethod
+    def __validate_body(body: Dict) -> List[float]:
+        if "numbers" not in body:
+            raise Exception("body mal formatado")
+
+        input_data = body["numbers"]
+        return input_data
+
+    def __calculate_variance(self, numbers: List[float]) -> float:
+        variance = self.__driver_handler.variance(numbers)
+
+        return variance
+
+    @staticmethod
+    def __calculate_multiplication(numbers: List[float]) -> float:
+        multiplication = 1
+        for number in numbers:
+            multiplication *= number
+
+        return multiplication
+
+    @staticmethod
+    def __verify_results(variance: float, multiplication: float) -> None:
+        if variance < multiplication:
+            raise Exception("Falha no processo: Variância menor que multiplicação")
+
+    @staticmethod
+    def __format_response(variance: float) -> Dict:
+        return {
+            "data": {
+                "Calculator": 3,
+                "value": variance,
+                "Success": True
+            }
+        }
